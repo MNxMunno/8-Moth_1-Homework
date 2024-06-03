8dffff813b896c70691de2e9d52bb85028c90cd4
MNxMunno <bilolhuzayfa@gmail.com>
2024-06-04 03:31:34 +0500

    change

--- src/routers/wishlist/Wishlist.jsx
+++ src/routers/wishlist/Wishlist.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // import useStore from "../../context/wishlist";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleWishlist } from "../../lib/action/action";
 
 const Wishlist = () => {
   const wishlist = useSelector((s) => s.wishlist);
+  const dispatch = useDispatch();
   console.log(wishlist);
   const card = wishlist.map((el) => (
     <div className="cards" key={el.id}>
@@ -14,7 +16,7 @@ const Wishlist = () => {
       <div className="card cart">
         <h2 title={el.title}>{el.title}</h2>
         <p>{(el.price * 12730).brm()} UZS</p>
-        <button onClick={() => heart(el)}>Remove</button>
+        <button onClick={() => dispatch(toggleWishlist(el))}>Remove</button>
       </div>
     </div>
   ));
@@ -22,7 +24,7 @@ const Wishlist = () => {
   return (
     <section className="wishlist">
       <div className="container">
-        {bear.length === 0 ? (
+        {wishlist.length === 0 ? (
           <h1 style={{ textAlign: "center" }}>404</h1>
         ) : (
           <div className="content">{card}</div>
